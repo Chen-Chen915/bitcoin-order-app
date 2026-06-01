26e6a09b1ae67142f3ad82a7ef8fc92fc5bc260c
Chen Chen <e1671983@u.nus.edu>
2026-06-02 00:28:19 +0800

    lint, dast,sast

--- scripts/zap-script.sh
+++ scripts/zap-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+
 docker pull zaproxy/zap-stable
-docker run --rm -v "$(pwd):/zap/wrk" zaproxy/zap-stable zap-baseline.py \
-  -t https://kenken64.github.io/bitcoin-order-app \
-  -r /zap/wrk/zap_report.html
+docker run -i zaproxy/zap-stable zap-baseline.py -t "https://kenken64.github.io/bitcoin-order-app/" -l PASS > zap_baseline_report.html
+
 echo $? > /dev/null
